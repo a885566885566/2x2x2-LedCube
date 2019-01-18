6162bf6812903da0796cd8ed7c65a08374bdcab1
a885566885566 <a885566885566@gmail.com>
2019-01-18 21:55:19 +0800

    Fixed votage limit bug

--- ledControllor/ledControllor.ino
+++ ledControllor/ledControllor.ino
@@ -6,18 +6,18 @@
 
 #define layer1_Pin A0
 #define layer2_Pin A1
-#define ledA_Pin 3
+#define ledA_Pin 6
 #define ledB_Pin 5
 #define ledC_Pin 10
 #define ledD_Pin 9
 
 #define ledVotage 2.7
 #define pwmMax 255
-const int ledVotageLimit = (5-ledVotage)*255/5;
-const char ledMap[2][2][2] = {
+const int ledVotageLimitPWM = 255-255*ledVotage/5;
+const short ledMap[2][2][2] = {
     {{0x100, 0x101},{0x110, 0x111}},
     {{0x000, 0x001},{0x010, 0x011}}
-}
+};
 /* HyperIndex
  *  layer0
  *   0 00 | 0 01
@@ -28,8 +28,15 @@ const char ledMap[2][2][2] = {
  */
 
 
-volatile char ledState[8] = {0};
+volatile short ledState[8] = {0};
 volatile bool ledLayerState = true;
+
+void anime_spark(int times=10, int fre=800, bool speedUp = true);
+void anime_pointMoving(int times=100, int fre=300);
+void anime_triangleMoving(int times=100, int fre=300);
+void anime_circleRuning(int times=100, int fre=300);
+void anime_lightUpSmooth();
+void anime_planeFlip(int times=100, int fre=300);
 
 void setup() {
   ISR_disable();
@@ -41,17 +48,14 @@ void setup() {
   pinMode(ledB_Pin, OUTPUT);
   pinMode(ledC_Pin, OUTPUT);
   pinMode(ledD_Pin, OUTPUT);
+  Serial.begin(9600);
   ISR_enable();
 }
-
-void loop() {
     
-    anime1();
+void loop() {
+    anime_spark(10, 1000, false);
 }
 
-/*
- * spark
- */
 void anime_spark(int times=10, int fre=800, bool speedUp = true){
     const int minimumFre = 10;
     for(int i = 0;i<times; i++){
@@ -68,11 +72,9 @@ void anime_spark(int times=10, int fre=800, bool speedUp = true){
     }
 }
 //getVotage( (sin(millis()/period)+1)*120 );
-/*
- * Point moving
- */
+
 void anime_pointMoving(int times=100, int fre=300){
-    static char index = 0x101;
+    static short index = 0x101;
     for(int i = 0; i<times; i++){
         modifyLedByHyperIndex(index, 0);
         randomChoose(&index);
@@ -81,12 +83,9 @@ void anime_pointMoving(int times=100, int fre=300){
     }
 }
 
-/*
- * Triangle moving
- */
 void anime_triangleMoving(int times=100, int fre=300){
-    static char head = 0x000;
-    static char tail = 0x001;
+    static short head = 0x000;
+    static short tail = 0x001;
     for(int i = 0; i < times; i++){
         modifyLedByHyperIndex(tail, 0);
         tail = head;
@@ -97,10 +96,10 @@ void anime_triangleMoving(int times=100, int fre=300){
 }
 
 void anime_circleRuning(int times=100, int fre=300){
-    static char upper = 0;
-    static char lower = 0;
+    static short upper = 0;
+    static short lower = 0;
     static bool layer = false;
-    const char arr[4] = {0x00, 0x01, 0x11, 0x10};
+    const short arr[4] = {0x00, 0x01, 0x11, 0x10};
     for(int i=0;i<times;i++){
         modifyLedByHyperIndex((layer ? 0 : 0x100) | arr[upper], 255);
         modifyLedByHyperIndex((layer ? 0x100 : 0) | arr[lower], 255);
@@ -112,23 +111,23 @@ void anime_circleRuning(int times=100, int fre=300){
 }
 
 void anime_lightUpSmooth(){
-    static 
+    //static 
 }
 void anime_planeFlip(int times=100, int fre=300){
-    static char plane[4] = {0x000, 0x001, 0x010, 0x011};
-    static char edge[2] = {0, 0};
+    static short plane[4] = {0x000, 0x001, 0x010, 0x011};
+    static short edge[2] = {0, 0};
     for(int w=0; w<times; w++){
         // Choose two points on the same edge
-        edge[0] = (char)random(0, 3);
-        char err = 0;
+        edge[0] = (short)random(0, 3);
+        short err = 0;
         do{
-            edge[1] = (char)random(0, 3);
+            edge[1] = (short)random(0, 3);
             err = plane[edge[0]]^plane[edge[1]];
         }while( !(err==1 || err==2 || err==6) );     // Get a index which not equal to the other one, and must be the same side
 
         // Get the bit on the selected edge which need to be change
-        static diffEdge[2]={0}, index=0;
-        char mutual = err;
+        static short diffEdge[2]={0}, index=0;
+        short mutual = err;
         for(int i = 1; i<=4; i*=2)
             if(i != mutual) diffEdge[index++] = i;   // diffEdge = 0x001, 0x010, 0x100
             
@@ -151,34 +150,34 @@ void anime_planeFlip(int times=100, int fre=300){
         updateLedByHyperIndexArray(plane, 4, 0);
     }
 }
-bool matched(char *arr, int len, char target){
+bool matched(short *arr, int len, short target){
     for(int i=0; i<len; i++)
         if(arr[i] == target) return true;
     return false;
 }
-void updateLedByHyperIndexArray(char *arr, char len, char state){
+void updateLedByHyperIndexArray(short *arr, short len, short state){
     for(int i=0; i<len; i++)
         modifyLedByHyperIndex(arr[i], state);
 }
-void randomChoose(char *index){
+void randomChoose(short *index){
     /*
      * index   ->  0x111
      * select  ->  0x010
      * mask    ->  0x101
      * output  ->  0x101
      */
-    char rnd = (char)random(0, 2);
-    char select = 1 << rnd; // Offset to target bit
+    short rnd = (short)random(0, 2);
+    short select = 1 << rnd; // Offset to target bit
     *index ^= select;      // Combine the two data
 }
 
-inline char getVotage(char state){
-    return ledVotageLimit + (pwmMax-ledVotageLimit)*state/pwmMax;
+inline short getVotage(short state){
+    return pwmMax - (pwmMax - ledVotageLimitPWM)*state/pwmMax;
 }
 
-void modifyLedByHyperIndex(char index, char state){
+void modifyLedByHyperIndex(short index, short state){
     if(index <= 0x111) return;
-    char offset = (index >> 2) ? 4 : 0;
+    short offset = (index >> 2) ? 4 : 0;
     index &= 0x11;
     if(index == 0x00)       ledState[offset  ] = getVotage(state);
     else if(index == 0x01)  ledState[offset+1] = getVotage(state);
@@ -191,10 +190,10 @@ void ISR_enable()
   TCCR2A = 0;
   TCCR2B = 0; 
   TCCR2B |= (1<<WGM22);  // CTC mode; Clear Timer on Compare
-  TCCR2B |= (1<<CS21);  // Prescaler == 8 ||(1<<CS30)
+  TCCR2B |= (1<<CS22) | (1<<CS20);  // Prescaler == 8 ||(1<<CS30)
   TIMSK2 |= (1 << OCIE2A);  // enable CTC for TIMER1_COMPA_vect
   TCNT2=0;  // counter 歸零 
-  OCR2A = 10000;
+  OCR2A = 65535;
 }
 
 void ISR_disable()
