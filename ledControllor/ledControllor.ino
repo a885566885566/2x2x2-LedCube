2b19d14c9934707b9068f45feaffebc32c35901f
a885566885566 <a885566885566@gmail.com>
2019-01-19 01:17:21 +0800

    Fixed numeral issue

--- ledControllor/ledControllor.ino
+++ ledControllor/ledControllor.ino
@@ -11,12 +11,12 @@
 #define ledC_Pin 10
 #define ledD_Pin 9
 
-#define ledVotage 2.7
+#define ledVotage 0.5
 #define pwmMax 255
 const int ledVotageLimitPWM = 255-255*ledVotage/5;
 const short ledMap[2][2][2] = {
-    {{0x100, 0x101},{0x110, 0x111}},
-    {{0x000, 0x001},{0x010, 0x011}}
+    {{B100, B101},{B110, B111}},
+    {{B000, B001},{B010, B011}}
 };
 /* HyperIndex
  *  layer0
@@ -34,7 +34,7 @@ volatile bool ledLayerState = true;
 void anime_spark(int times=10, int fre=800, bool speedUp = true);
 void anime_pointMoving(int times=100, int fre=300);
 void anime_triangleMoving(int times=100, int fre=300);
-void anime_circleRuning(int times=100, int fre=300);
+void anime_circleRuning(int times=100, int fre=100);
 void anime_lightUpSmooth();
 void anime_planeFlip(int times=100, int fre=300);
 
@@ -49,11 +49,16 @@ void setup() {
   pinMode(ledC_Pin, OUTPUT);
   pinMode(ledD_Pin, OUTPUT);
   Serial.begin(9600);
+  updateAllLeds(0);
   ISR_enable();
 }
     
 void loop() {
-    anime_spark(10, 1000, false);
+    //anime_spark(10, 100, true);
+    //anime_pointMoving();
+    //anime_triangleMoving();
+    //anime_circleRuning();
+    anime_planeFlip();
 }
 
 void anime_spark(int times=10, int fre=800, bool speedUp = true){
@@ -62,9 +67,8 @@ void anime_spark(int times=10, int fre=800, bool speedUp = true){
         for(int k = 0; k<8; k++)
             ledState[k] = getVotage( 255 );
         delay(fre);
-        for(int k = 0; k<8; k++){
+        for(int k = 0; k<8; k++)
             ledState[k] = getVotage( 0 );
-        }
         delay(fre);
         if(speedUp){
             fre = minimumFre + (times-i)*(fre-minimumFre)/times;
@@ -74,39 +78,42 @@ void anime_spark(int times=10, int fre=800, bool speedUp = true){
 //getVotage( (sin(millis()/period)+1)*120 );
 
 void anime_pointMoving(int times=100, int fre=300){
-    static short index = 0x101;
+    short index = B101;
+    Serial.println(index, BIN);
     for(int i = 0; i<times; i++){
         modifyLedByHyperIndex(index, 0);
-        randomChoose(&index);
+        index = randomChoose(index);
         modifyLedByHyperIndex(index, 255);
         delay(fre);
     }
 }
 
 void anime_triangleMoving(int times=100, int fre=300){
-    static short head = 0x000;
-    static short tail = 0x001;
+    static short head = B000;
+    static short tail = B001;
     for(int i = 0; i < times; i++){
         modifyLedByHyperIndex(tail, 0);
         tail = head;
-        randomChoose(&head);
+        head = randomChoose(head);
         modifyLedByHyperIndex(head, 255);
         delay(fre);
     }
 }
 
-void anime_circleRuning(int times=100, int fre=300){
+void anime_circleRuning(int times=100, int fre){
     static short upper = 0;
     static short lower = 0;
     static bool layer = false;
-    const short arr[4] = {0x00, 0x01, 0x11, 0x10};
+    const short arr[4] = {B00, B01, B11, B10};
     for(int i=0;i<times;i++){
-        modifyLedByHyperIndex((layer ? 0 : 0x100) | arr[upper], 255);
-        modifyLedByHyperIndex((layer ? 0x100 : 0) | arr[lower], 255);
-        upper = (upper>=4 ? 0 : upper+1);
+        upper = (upper>=3 ? 0 : upper+1);
         lower = (lower<=0 ? 3 : lower-1);
+        modifyLedByHyperIndex((layer ? 0 : B100) | arr[upper], 255);
+        modifyLedByHyperIndex((layer ? B100 : 0) | arr[lower], 255);
         delay(fre);
-        if(i%(300/10)) layer = !layer;
+        modifyLedByHyperIndex((layer ? 0 : B100) | arr[upper], 0);
+        modifyLedByHyperIndex((layer ? B100 : 0) | arr[lower], 0);
+        //if(i%(300/10)) layer = !layer;
     }
 }
 
@@ -114,14 +121,14 @@ void anime_lightUpSmooth(){
     //static 
 }
 void anime_planeFlip(int times=100, int fre=300){
-    static short plane[4] = {0x000, 0x001, 0x010, 0x011};
+    static short plane[4] = {B000, B001, B010, B011};
     static short edge[2] = {0, 0};
     for(int w=0; w<times; w++){
         // Choose two points on the same edge
-        edge[0] = (short)random(0, 3);
+        edge[0] = (short)random(0, 4);
         short err = 0;
         do{
-            edge[1] = (short)random(0, 3);
+            edge[1] = (short)random(0, 4);
             err = plane[edge[0]]^plane[edge[1]];
         }while( !(err==1 || err==2 || err==6) );     // Get a index which not equal to the other one, and must be the same side
 
@@ -129,7 +136,7 @@ void anime_planeFlip(int times=100, int fre=300){
         static short diffEdge[2]={0}, index=0;
         short mutual = err;
         for(int i = 1; i<=4; i*=2)
-            if(i != mutual) diffEdge[index++] = i;   // diffEdge = 0x001, 0x010, 0x100
+            if(i != mutual) diffEdge[index++] = i;   // diffEdge = B001, B010, B100
             
         // Determine if the bit order is wrong
         bool wrongOrder = matched(plane, 4, edge[0]^diffEdge[0]);
@@ -155,34 +162,31 @@ bool matched(short *arr, int len, short target){
         if(arr[i] == target) return true;
     return false;
 }
-void updateLedByHyperIndexArray(short *arr, short len, short state){
-    for(int i=0; i<len; i++)
-        modifyLedByHyperIndex(arr[i], state);
-}
-void randomChoose(short *index){
-    /*
-     * index   ->  0x111
-     * select  ->  0x010
-     * mask    ->  0x101
-     * output  ->  0x101
-     */
-    short rnd = (short)random(0, 2);
+short randomChoose(short index){
+    short rnd = (short)random(0, 3);
     short select = 1 << rnd; // Offset to target bit
-    *index ^= select;      // Combine the two data
+    return index ^ select;      // Combine the two data
 }
 
 inline short getVotage(short state){
     return pwmMax - (pwmMax - ledVotageLimitPWM)*state/pwmMax;
 }
-
+void updateAllLeds(short state){
+    for(int i=0; i<8; i++)
+        ledState[i] = getVotage(state);
+}
+void updateLedByHyperIndexArray(short *arr, short len, short state){
+    for(int i=0; i<len; i++)
+        modifyLedByHyperIndex(arr[i], state);
+}
 void modifyLedByHyperIndex(short index, short state){
-    if(index <= 0x111) return;
+    if(index > B111) return;
     short offset = (index >> 2) ? 4 : 0;
-    index &= 0x11;
-    if(index == 0x00)       ledState[offset  ] = getVotage(state);
-    else if(index == 0x01)  ledState[offset+1] = getVotage(state);
-    else if(index == 0x10)  ledState[offset+2] = getVotage(state);
-    else if(index == 0x011) ledState[offset+3] = getVotage(state);
+    index &= B11;
+    if(index == B00)       ledState[offset  ] = getVotage(state);
+    else if(index == B01)  ledState[offset+1] = getVotage(state);
+    else if(index == B10)  ledState[offset+2] = getVotage(state);
+    else if(index == B11)  ledState[offset+3] = getVotage(state);
 }
 
 void ISR_enable()
@@ -193,7 +197,7 @@ void ISR_enable()
   TCCR2B |= (1<<CS22) | (1<<CS20);  // Prescaler == 8 ||(1<<CS30)
   TIMSK2 |= (1 << OCIE2A);  // enable CTC for TIMER1_COMPA_vect
   TCNT2=0;  // counter 歸零 
-  OCR2A = 65535;
+  OCR2A = 1000;
 }
 
 void ISR_disable()
